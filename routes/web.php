86b8bb61363817055f8d588f5cf8ffb40075a3d3
ByJosefh11 <josefernandezherrera11@gmail.com>
2023-02-13 13:43:28 +0100

    actulizado la plantilla product

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/products', function () {
+    return view('products');
+})->name('products');
