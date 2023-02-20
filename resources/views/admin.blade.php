5e8d2d23e61bd7a397721d568261b37a11782bb0
Danielreyezzz <dangioparefape@gmail.com>
2023-02-20 10:43:03 +0100

    fix:(password hash in dashboard)

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -86,7 +86,7 @@
         <main>
             <div class="container-fluid px-4">
                 <h1 class="mt-4">Dashboard</h1>
-             
+
                 <ol class="breadcrumb mb-4">
                     <li class="breadcrumb-item active">Products</li>
                 </ol>
